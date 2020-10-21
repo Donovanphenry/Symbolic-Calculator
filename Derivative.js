6f741ef1b38d6cadce0b55d116e31545bfaed02d
Donovan Henry <donovanphenry@gmail.com>
2020-10-21 14:04:31 -0700

    Updated most functions to have descriptive comments

--- Derivative.js
+++ Derivative.js
@@ -13,28 +13,38 @@ class Derivative
         this.derivatives["ln"] = "(1 / ?)";
     }
 
+	/*
+		==============================================================================================================================
+		Calculate acts as the main function to determine the individual sub-functions and to determine what kind of derivative
+		each sub-function will be. For instance, if sin(tan(x)) is passed into it, all sub-functions will be sin(tan(x)), tan(x)
+		and x. Calculate will also determine that we have a chain rule function, and will route the derivative to the right derivative
+		rule, namely the chain rule function.
+		==============================================================================================================================
+	*/
     calculate ()
     {
+
         let funcSub = "";
 		let allFuncStack = [];
 		let funcStartStack = [];
 		let funcEndQ = [];
 		let prFunctionsArr = [];
 		let allFunctionsDerivArr = [];
-		let derivativesArr = [];
         let start = 0;
         let end = 0;
 
         funcStartStack.push(0);
 
+		// ====================================================================================================================================================
         // Iterate through each character in the for loop. If we've found an opening parenthesis, that'll mean we've found a new inner function,
         // so we want to take note of that index and add it to the startStack, as that is the start of a new function. Same ideology with when we 
-        // find a closing parenthesis. Once we reach this closing parentheses, there are two things to add to the allFuncStacj. We need to out
+        // find a closing parenthesis. Once we reach this closing parentheses, there are two things to add to the allFuncStack. We need to add
         // the outer function (e.g. sin( f(x) ) ) and the inner function ( f(x) ). To achieve this we add the current index i to the funcEndQ
         // and the (i + 1) index. We do this because of how the substring function works. String.substring(startIndex, endIndex) goes from the startIndex,
         // to the endIndex - 1, so in order to add the current substring to the stack, we need to go from the top of the funcStartStack to the current index
         // i. After this if statement ends, we'll have added the inner and outer functions to the allFuncStack and we'll have added the outer function to
-        // the prFunctionsArr array. 
+		// the prFunctionsArr array. 
+		// ====================================================================================================================================================
         for (let i = 0; i < this.posFunc.length; i++)
 		{
 			if (this.posFunc.charAt(i) == '(')
@@ -53,10 +63,10 @@ class Derivative
 				}
 			}
 			
-			if (funcEndQ.length == 0 && funcStartStack.length == 0 && i < 1)
-			{
+			// if (funcEndQ.length == 0 && funcStartStack.length == 0 && i < 1)
+			// {
 				
-			}
+			// }
         }
         
         //We need to add the last character in the string to catch the entire outer function
@@ -94,6 +104,11 @@ class Derivative
 
     }
 
+	/*
+		oneDepDeriv takes a string expression and returns the derivative of the outer most function, hence the name "oneDependentDerivative", it only depends
+		on the outermost function and doesn't worry about what's inside. It accomplishes this by finding the first instance of an opening parenthesis which symbolizes
+		that we've found the inner function, and to have found the inner function would mean we've traversed over the entire outer function. 
+	*/
     oneDepDeriv(expr)
 	{
 		for (let k = 0; k < expr.length; k++)
@@ -102,7 +117,10 @@ class Derivative
 		
 		return "d/dx(" + expr + ")";
 	}
-    
+	
+	/*
+		
+	*/
     add2Stack(funcStartStack, funcEndQ, functions, parFunction)
 	{
 		while ((funcEndQ.length == 0) == false)
@@ -171,7 +189,7 @@ class Derivative
 
 function main ()
 {
-    let positionFunction = "sin(x)tan(x)sec(x)";
+    let positionFunction = "sin(x)ln(x)";
     console.log("==================== USER INPUT ==========================");
     console.log("Original/Position Function: " + positionFunction);
     let d = new Derivative(positionFunction);
